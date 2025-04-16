aedc56cba61d8436306f04f1e09b469b4fef58a7
ducsiukap <DucPV.contact@gmail.com>
2025-04-16 15:37:19 +0700

    quen chua add :)

--- src/context/Authentication.js
+++ src/context/Authentication.js
@@ -1,44 +1,44 @@
-import { createContext, useEffect, useState } from "react";
-import { findUser } from "../models/Users";
+  import { createContext, useEffect, useState } from "react";
+  import { findUser } from "../models/Users";
 
-const AuthContext = createContext(null);
+  const AuthContext = createContext(null);
 
-function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+  function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setLoading(true);
-    const saved = localStorage.getItem('user');
-    if (saved) {
-      setUser(JSON.parse(saved));
+    useEffect(() => {
+      setLoading(true);
+      const saved = localStorage.getItem('user');
+      if (saved) {
+        setUser(JSON.parse(saved));
+      }
+      setLoading(false);
+    }, [])
+
+    const login = ({usrn, pwrd}) => {
+      const u = findUser({ usrn, pwrd });
+      // console.log(u);
+      if (u) {
+        setUser(u);
+        localStorage.setItem('user', JSON.stringify(u));
+        return true; 
+      }
+      return false;
     }
-    setLoading(false);
-  }, [])
 
-  const login = ({usrn, pwrd}) => {
-    const u = findUser({ usrn, pwrd });
-    // console.log(u);
-    if (u) {
-      setUser(u);
-      localStorage.setItem('user', JSON.stringify(u));
-      return true; 
-    }
-    return false;
+    const loggedIn = !!user;
+
+    const logout = () => {
+      setUser(null);
+      localStorage.removeItem('user');
+    };
+
+    return (
+      <AuthContext.Provider value={{ user, loggedIn, login, logout, loading }}>
+        {children}
+      </AuthContext.Provider>
+    );
   }
 
-  const loggedIn = !!user;
-
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem('user');
-  };
-
-  return (
-    <AuthContext.Provider value={{ user, loggedIn, login, logout, loading }}>
-      {children}
-    </AuthContext.Provider>
-  );
-}
-
-export {AuthContext, AuthProvider};
+  export {AuthContext, AuthProvider};
